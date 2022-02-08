32ab0be036f557726002b17e06284ddb34dddab1
Priboy <priboy313@gmail.com>
2022-02-08 16:33:21 +0300

    рейтинговая работа 2, полный код

--- Второй семестр/Рейтинговая работа 2/Воробьев_КА.py
+++ Второй семестр/Рейтинговая работа 2/Воробьев_КА.py
@@ -20,9 +20,57 @@ def get_unicode_list(src_string:str):
 	return _unicode_list
 
 
+def merge_sort(src_unicode_list:list):
+	"""Сортировка слиянием"""
+	_sorted_list = src_unicode_list.copy()
+	
+	if len(_sorted_list) > 1:
+		mid = len(_sorted_list) // 2
+		left = _sorted_list[:mid]
+		right = _sorted_list[mid:]
+		
+		merge_sort(left)
+		merge_sort(right)
+		
+		i = j = k = 0
+		
+		while i < len(left) and j < len(right):
+			if left[i] < right[j]:
+				_sorted_list[k] = left[i]
+				i+=1
+			else:
+				_sorted_list[k] = right[j]
+				j+=1
+			k += 1
+		
+		while i < len(left):
+			_sorted_list[k] = left[i]
+			i+=1
+			k+=1
+		
+		while j < len(right):
+			_sorted_list[k] = right[j]
+			j+=1
+			k+=1
+		
+	return _sorted_list
+
+
+def bubble_sort(src_unicode_list:list):
+	"""Сортировка пузырьком"""
+	_sorted_list = src_unicode_list.copy()
+	
+	for i in range(len(_sorted_list)-1):
+		for j in range(len(_sorted_list)-1):
+			if _sorted_list[j] > _sorted_list[j+1]:
+				_sorted_list[j], _sorted_list[j+1] = _sorted_list[j+1], _sorted_list[j]
+	
+	return _sorted_list
+
+
 def sort_ascending(src_unicode_list:list):
 	"""Сортировка по возрастанию"""
-	_sorted_list = []
+	_sorted_list = bubble_sort(src_unicode_list)
 	_quo_parity = "чётное"
 	_sorted_type = "по возрастанию"
 	return _sorted_list, _quo_parity, _sorted_type
@@ -30,10 +78,10 @@ def sort_ascending(src_unicode_list:list):
 
 def sort_descending(src_unicode_list:list):
 	"""Сортировка по убыванию"""
-	_sorted_list = []
+	_sorted_list = merge_sort(src_unicode_list)
 	_quo_parity = "нечётное"
 	_sorted_type = "по убыванию"
-	return _sorted_list, _quo_parity, _sorted_type
+	return _sorted_list[::-1], _quo_parity, _sorted_type
 
 
 def get_average(src_list:list):
@@ -51,7 +99,7 @@ def get_square(src_list:list):
 	_sum = 0
 	
 	for i in range(0, len(src_list)):
-		_sum += src_list[i]**2
+		_sum += int(src_list[i])**2
 	
 	return round((_sum / len(src_list))**0.5, 3)
 
@@ -65,17 +113,15 @@ def main():
 	square = get_square(unicode_list)
 	
 	with open("result.txt", "w", encoding="utf-8") as file:
-		file.write(
-f"""1. Исходные данные: {src_data[0]}; ID: {src_data[1]}
+		file.write(f"""
+1. Исходные данные: {src_data[0]}; ID: {src_data[1]}
 2. {quotient}
 3. Направление сортировки: {sorted_type}, так как число {quotient} - {quo_parity}
 4. Набор данных: {unicode_list}
-5. Отсортированный {sorted_type} набор данных {sorted_list}
+5. Отсортированный {sorted_type} набор данных: {sorted_list}
 6. Среднее арифметическое значение: {average}
-7. Среднее квадратическое значение: {square}""")
+7. Среднее квадратическое значение: {square}"""[1:])
 	
-	# print(f"\n{src_data = }\n{quotient = }\n{unicode_list = }\n{sorted_list = }\n{quo_parity = }\n{sorted_type = }")
-
 
 if __name__=='__main__':
 	main()
