fb0a2a42de6aeccfed94063b72b0a316294b63ea
Priboy313 <62994406+Priboy313@users.noreply.github.com>
2022-05-23 12:41:49 +0300

    Второй семестр, исходник для практики.

--- __nonfile.py
+++ __nonfile.py
@@ -1,7 +1,30 @@
+l1 = [1, 2, 3]
+l2 = [4, 5, 6]
+
+l3 = [1, 2, 3]
+l4 = [4, 5]
+
+# def mymap(func, *lists) -> list:
+# 	_lists = sorted(lists, key=len)
+# 	result = []
+#
+# 	for x in range(0, len(_lists[0])):
+# 		result.append(func(_lists[0][x], _lists[1][x]))
+#
+# 	return result
 
 
+def mymap(func, *args) -> list:
+	match len(args):
+		case 2:
+			_result = map(func, args[0], args[1])
+		case _:
+			_result = map(func, args[0])
+	
+	return list(_result)
 
 
-
+print(mymap(lambda x, y: x + y, l2, l1))
+print(mymap(lambda x, y: x + y, l3, l4))
 
 
---
+++ Второй семестр/Практика/ВоробьевКА.py
@@ -0,0 +1,83 @@
+# SimpleAnalysis
+import random
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tkr
+
+
+def get_series() -> pd.Series:
+	"""Создание объекта Series"""
+	_series = []
+	for i in range(1000):
+		_series.append(random.randint(-300, 300))
+	
+	return pd.Series(_series)
+
+
+def set_series_graph(series:pd.Series) -> None:
+	"""Построение графика объекта series"""
+	_line = series.plot(kind="line", title="series_line")
+
+
+def set_series_histogram(series:pd.Series) -> None:
+	"""Построение гистограммы объекта series"""
+	_rounded_series = np.ceil(series / 100) * 100
+	_hist = _rounded_series.plot(kind="hist", title="series_hist")
+	_hist.xaxis.set_major_locator(tkr.MultipleLocator(500))
+	_hist.xaxis.set_minor_locator(tkr.MultipleLocator(200))
+	_hist.set_xlim([-300, 300])
+
+
+def get_dataframe(series:pd.Series) -> pd.DataFrame:
+	"""Создание объекта dataframe"""
+	_dataframe = pd.DataFrame({"Series": series})
+	_dataframe["Ascending"] = series.sort_values(ascending=True, ignore_index=True)
+	_dataframe["Descending"] = series.sort_values(ascending=False, ignore_index=True)
+	
+	return _dataframe
+
+
+def set_dataframe_graph(dataframe:pd.DataFrame) -> None:
+	"""Построение графика объекта dataframe"""
+	_line = dataframe.drop(["Series"], axis=1).plot(kind="line", title="dataframe_line")
+
+
+def main():
+	# Получение объекта series
+	series = get_series()
+
+	# Рассчёт стандартных характеристик объекта series
+	series_min = min(series)
+	series_max = max(series)
+	series_repeats = len(series) - len(series.unique())
+	series_sum = sum(series)
+	print(
+		f"Минимальное значение series: {series_min}\n"
+		f"Максимальное значение series: {series_max}\n"
+		f"Повторияющихся значений в series: {series_repeats}\n"
+		f"Сумма значений series: {series_sum}\n"
+		)
+	
+	# Построение графика объекта series
+	set_series_graph(series)
+	plt.show()
+
+	# Построоение гистограммы объекта series
+	set_series_histogram(series)
+	plt.show()
+
+	# Формирование объекта dataframe
+	dataframe = get_dataframe(series)
+	
+	# Построение графика dataframe
+	set_dataframe_graph(dataframe)
+	plt.show()
+
+
+if __name__ == "__main__":
+	main()
+
+
+
+
